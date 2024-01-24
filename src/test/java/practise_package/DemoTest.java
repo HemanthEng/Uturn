9c8770eef796ebbe772a77a718e078999c34eafb
Hems <qa.engineer@gmail.com>
2024-01-24 15:47:13 +0530

    Design phase is ready Boss

---
+++ src/test/java/practise_package/DemoTest.java
@@ -0,0 +1,23 @@
+package practise_package;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+
+	@Test(groups="smoke")
+	public void demoTest()
+	{
+		System.out.println("this method is DemoTest==>> smoke test");
+	}
+	
+	@Test(groups="regression")
+	public void demoTest1()
+	{
+		System.out.println("this method is DemoTest1==>>Regression Test");
+	}
+	@Test(groups="regression")
+	public void demoTest2()
+	{
+		System.out.println("this method is DemoTest2==>> Regression Test");
+	}
+}
---
+++ src/test/java/practise_package/Practise_test.java
@@ -0,0 +1,23 @@
+package practise_package;
+
+import org.testng.annotations.Test;
+
+public class Practise_test {
+
+	@Test(groups="smoke")
+	public void practiseTest()
+	{
+		System.out.println("this method is PractiseTest==>> smoke test");
+	}
+	
+	@Test(groups="regression")
+	public void practiseTest1()
+	{
+		System.out.println("this method is PractiseTest1==>>Regression Test");
+	}
+	@Test(groups="regression")
+	public void practiseTest2()
+	{
+		System.out.println("this method is PractiseTest2==>> Regression Test");
+	}
+}

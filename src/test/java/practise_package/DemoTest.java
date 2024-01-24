7ee8ff55d58930ce4e4d94d35586deaae0551e36
HemanthEng <156580268+HemanthEng@users.noreply.github.com>
2024-01-24 18:27:19 +0530

    Update DemoTest.java
    newly updated

--- src/test/java/practise_package/DemoTest.java
+++ src/test/java/practise_package/DemoTest.java
@@ -10,24 +10,5 @@ public class DemoTest {
 		System.out.println("this method is DemoTest==>> smoke test");
 	}
 	
-	@Test(groups="regression")
-	public void demoTest1()
-	{
-		System.out.println("this method is DemoTest1==>>Regression Test");
-	}
-	@Test(groups="regression")
-	public void demoTest2()
-	{
-		System.out.println("this method is DemoTest2==>> Regression Test");
-	}
-	@Test(groups="smoke")
-	public void demoTest3()
-	{
-		System.out.println("this method is DemoTest2==>> Regression Test");
-	}
-	@Test(groups="smoke")
-	public void demoTest4()
-	{
-		System.out.println("this method is DemoTest2==>> Regression Test");
-	}
+	
 }

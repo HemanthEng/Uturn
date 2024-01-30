265bee2de9d76c4e549a0246d882b0b564bff1c0
Hems <qa.engineer@gmail.com>
2024-01-30 09:57:53 +0530

    created a new project and commiting

--- src/test/java/practise_package/mastercreated.java
+++ src/test/java/practise_package/mastercreated.java
@@ -4,7 +4,7 @@ import org.testng.annotations.Test;
 
 public class mastercreated {
 	@Test(groups="smoke")
-	public void masterTest(String br)
+	public void masterTest()
 	{
 		String BROWSER = System.getProperty("browser");
 		String Env = System.getProperty("Env");
